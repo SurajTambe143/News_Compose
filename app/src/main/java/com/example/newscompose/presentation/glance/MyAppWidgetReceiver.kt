0ce8c9eae867db69c80eab13bc6ca6e215a776c8
Suraj Tambe <suraj.tambe@neosoftmail.com>
2024-02-20 17:20:58 +0530

    simple glance impl

---
+++ app/src/main/java/com/example/newscompose/presentation/glance/MyAppWidgetReceiver.kt
@@ -0,0 +1,10 @@
+package com.example.newscompose.presentation.glance
+
+import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.GlanceAppWidgetReceiver
+
+class MyAppWidgetReceiver : GlanceAppWidgetReceiver() {
+
+    // Let MyAppWidgetReceiver know which GlanceAppWidget to use
+    override val glanceAppWidget: GlanceAppWidget = MyAppWidget()
+}
